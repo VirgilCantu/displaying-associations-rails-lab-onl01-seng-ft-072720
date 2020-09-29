11a86480d8302066ebdd2bbcb16d1914edd586ef
VirgilCantu <cantu.sampsom@gmail.com>
2020-09-29 09:47:20 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@artist1 = Artist.create(name: "Disclosure")
+@artist1.songs.create(title: "You and Me")
+@artist2 = Artist.create(name: "53 Thieves")
+@artist2.songs.create(title: "3 AM")
